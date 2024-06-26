8537da1aa9ff894f1e1f09e1af31d543ee9738ad
Paulo de Pinho <prdepinho@gmail.com>
2024-06-26 11:08:52 -0300

    tweaks

--- init.lua
+++ init.lua
@@ -2,8 +2,8 @@
 
 vim.o.number = true
 vim.o.relativenumber = true
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
 vim.o.smartindent = true
 vim.o.wrap = false
 vim.o.cursorline = true
@@ -28,21 +28,24 @@ vim.keymap.set('i', '[<RETURN>', '[<RETURN>]<ESC>O')
 vim.keymap.set('v', '>', '>gv')
 vim.keymap.set('v', '<', '<gv')
 
+vim.keymap.set('t', '<c-w>k', '<Cmd>wincmd k<CR>')
+vim.keymap.set('t', '<c-w>j', '<Cmd>wincmd j<CR>')
+vim.keymap.set('t', '<c-w>h', '<Cmd>wincmd h<CR>')
+vim.keymap.set('t', '<c-w>l', '<Cmd>wincmd l<CR>')
+
 vim.api.nvim_create_autocmd('FileType', {
-    pattern = {'html', 'xml'},
-    callback = function(args)
-        vim.keymap.set('i', '><RETURN>', '><ESC>T<yWf>a</<ESC>pA<BACKSPACE>><ESC>F<i<RETURN><ESC>O')
-        vim.keymap.set('i', '><SPACE>', '><ESC>T<yWf>a</<ESC>pA<BACKSPACE>><ESC>F<i')
-        vim.o.tabstop = 2
-        vim.o.shiftwidth = 2
-    end
+  pattern = {'html', 'xml'},
+  callback = function(args)
+    vim.keymap.set('i', '><RETURN>', '><ESC>T<yWf>a</<ESC>pF<i<RETURN><TAB>')
+    vim.keymap.set('i', '><SPACE>', '><ESC>T<yWf>a</<ESC>pF<i')
+  end
 })
 
 vim.api.nvim_create_autocmd('FileType', {
-    pattern = 'lua',
-    callback = function(args)
-        vim.keymap.set('i', 'do<RETURN>', 'doend<ESC>Fei<RETURN><ESC>O')
-        vim.keymap.set('i', 'then<RETURN>', 'thenend<ESC>Fei<RETURN><ESC>O')
-        vim.keymap.set('i', 'function<SPACE>', 'functionend<ESC>Fei<RETURN><ESC>kA<SPACE>')
-    end
+  pattern = 'lua',
+  callback = function(args)
+    vim.keymap.set('i', 'do<RETURN>', 'doend<ESC>Fei<RETURN><ESC>O')
+    vim.keymap.set('i', 'then<RETURN>', 'thenend<ESC>Fei<RETURN><ESC>O')
+    vim.keymap.set('i', 'function<SPACE>', 'functionend<ESC>Fei<RETURN><ESC>kA<SPACE>')
+  end
 })
